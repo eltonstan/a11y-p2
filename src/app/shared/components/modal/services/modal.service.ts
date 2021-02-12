ef3dfc84216b71ae0cc3c2de6b6ef72e68e381ad
Elton Standerski <eltonstan@yahoo.com.br>
2021-02-12 14:34:11 -0300

    Aula 01 a 05

---
+++ src/app/shared/components/modal/services/modal.service.ts
@@ -0,0 +1,33 @@
+import { ComponentFactory, ComponentFactoryResolver, ComponentRef, Injectable, Injector } from '@angular/core';
+import { BodyInjectorService } from '../../services/body-injector';
+import { ModalConfig } from '../interfaces/modal-config';
+import { ModalComponent } from '../modal.component';
+import { ModalRef } from '../models/model-ref';
+
+@Injectable()
+export class ModalService {
+
+    private componentFactory: ComponentFactory<ModalComponent>;
+
+    constructor(private componentFactoryResolver: ComponentFactoryResolver,
+        private injector: Injector,
+        private bodyInjectorService : BodyInjectorService) {
+        this.componentFactory = componentFactoryResolver.resolveComponentFactory(ModalComponent);
+    }
+
+    public open(config: ModalConfig): ModalRef {
+        const componentRef = this.createComponentRef();
+        componentRef.instance.config = config;
+        console.log(componentRef.instance);
+        console.log('open called');
+        this.bodyInjectorService.stackBeforeAppRoot(componentRef);
+        const modalRef = new ModalRef(componentRef);
+        componentRef.instance.modalRef = modalRef;
+        return modalRef;
+    }
+
+    private createComponentRef(): ComponentRef<ModalComponent> {
+        return this.componentFactory.create(this.injector);
+    }
+
+}
